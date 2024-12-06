bfeea8d5c7bd7d8c8ce39e6f1372766bc7991c41
Siddhant Rathi <you@sid20rathi@gmail.com>
2024-12-06 18:58:46 +0530

    feat: fix an error

--- src/components/global/join-group/index.tsx
+++ src/components/global/join-group/index.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Loader } from "@/components/global/loader"
 import { Button } from "@/components/ui/button"
 import { useJoinGroup } from "@/hooks/payment"
 
@@ -13,7 +12,7 @@ type JoinGroupPaymentFormProps = {
 export const JoinGroupPaymentForm = ({
   groupid,
 }: JoinGroupPaymentFormProps) => {
-  const { isPending, onPayToJoin } = useJoinGroup(groupid)
+  const {  onJoinFreeGroup } = useJoinGroup(groupid)
   return (
     <div className="flex flex-col gap-y-3">
       <CardElement
@@ -30,8 +29,8 @@ export const JoinGroupPaymentForm = ({
         }}
         className="bg-themeBlack border-[1px] border-themeGray outline-none rounded-lg p-3"
       />
-      <Button onClick={onPayToJoin}>
-        <Loader loading={isPending}>Pay Now</Loader>
+      <Button onClick={ onJoinFreeGroup}>
+        Pay Now
       </Button>
     </div>
   )
